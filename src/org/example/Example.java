9b0cdc21101d4f8c2868e4e4822f015240ebdb1e
VarshaVinoth <bvvarsha30111993@gmail.com>
2021-09-25 16:19:15 +0530

    Yog added code

--- src/org/example/Example.java
+++ src/org/example/Example.java
@@ -1,6 +1,10 @@
 package org.example;
 
 public class Example {
+	private void m2() {
+		System.out.println("Yog added code");
+	}
+	
 public static void main(String[] args) {
 	System.out.println("data");
 	System.out.println("sample");
